b723adbef77e06b49fe82af2f7fc00565761f04e
Ilya Mischenko <timur.demichev@yandex.ru>
2021-02-13 11:25:54 +0200

    feature/create second form

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,66 @@
-let formElement = document.querySelector('.popup');
+let formEdit = document.querySelector('.popup__profile-edit');
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_description');
+
+let formAdd = document.querySelector('.popup__new-place');
 
 let nameInfo = document.querySelector('.profile__title');
 let jobInfo = document.querySelector('.profile__subtitle');
 
 let edit = document.querySelector('.profile__edit-button');
 let close = document.querySelector('.popup__close-button');
+
+let add = document.querySelector('.profile__add-button');
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const photoContainerEl = document.querySelector('.photo-items')
+
+function render() {
+  const html = initialCards
+    .map(getItemHTML)
+    .join('')
+    photoContainerEl.insertAdjacentHTML("afterbegin", html)
+}
+
+function getItemHTML(item) {
+  return `<article class="photo-items__post">
+          <img class="photo-items__image" src="${item.link}" alt="${item.name}">
+          <div class="photo-items__description">
+            <h2 class="photo-items__title">${item.name}</h2>
+            <button type="button" class="photo-items__like-button"></button>
+          </div>
+          </article>`
+}
+
+render();
+
 
 
 function addInfo() {
@@ -21,24 +75,67 @@ function formSubmitHandler(evt) {
 }
 
 function showPopup() {
-  formElement.classList.add('popup_opened');
+  formEdit.classList.add('popup_opened');
+  nameInput.value = nameInfo.textContent;
+  jobInput.value = jobInfo.textContent;
+}
+
+function showPopup() {
+  formAdd.classList.add('popup_opened');
   nameInput.value = nameInfo.textContent;
   jobInput.value = jobInfo.textContent;
 }
 
 function closePopup() {
-  formElement.classList.remove('popup_opened');
+  formEdit.classList.remove('popup_opened');
+}
+
+function closePopup() {
+  formAdd.classList.remove('popup_opened');
 }
 
 
 edit.addEventListener('click', showPopup);
+add.addEventListener('click', showPopup);
 close.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', formSubmitHandler);
 
 
 
 
 
+
+
+
+// function addCards() {
+//   const html = initialCards
+//   .map(addEl)
+//   itemsTemplate.append(...html);
+// }
+
+// const templateElement = document.querySelector('#photo-items-template');
+
+// function addEl(item) {
+//   // const newElement = document.querySelector('#photo-items-template').content;
+//   const newElement = templateElement.content.cloneNode(true);
+//   const titleElement = newElement.querySelector('.photo-items__title');
+//   const imageElement = newElement.querySelector('.photo-items__image');
+//   titleElement.textContent = item.name;
+//   imageElement.src = item.link;
+//   imageElement.alt = item.name;
+//   return newElement;
+// }
+
+
+// function addEl(item) {
+//   const itemsTemplate = document.querySelector('#photo-items-template').content;
+//   const itemsElement = itemsTemplate.cloneNode(true);
+//   itemsElement.querySelector('.photo-items__image').textContent = itemsImage;
+//   itemsElement.querySelector('.photo-items__title').textContent = itemsTitle;
+//   photoItems.append(itemsElement);
+// }
+
+// addCards();
 
 
 
