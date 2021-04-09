704b608bac21ed25697c6b8ae9717c34d505665b
Ilya Mischenko <timur.demichev@yandex.ru>
2021-04-09 10:01:22 +0200

    test3

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,7 +1,7 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
-  constructor(popupSelector, popupImg, popupDescription) {
+  constructor(popupSelector) {
     super(popupSelector);
     this._image = this._popup.querySelector('.popup__img');
     this._title = this._popup.querySelector('.popup__description');
