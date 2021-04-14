1fe6089c4b5719b43e525da7d7e67145e74ba938
Ilya Mischenko <timur.demichev@yandex.ru>
2021-04-14 21:38:06 +0200

    fix: delete unusual constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -28,31 +28,10 @@ const initialCards = [
 const formEdit = document.querySelector('.popup_profile-edit');
 const nameInput = document.querySelector('.popup__field_type_name');
 const jobInput = document.querySelector('.popup__field_type_description');
-
 const formAdd = document.querySelector('.popup_new-place');
-const titleInput = document.querySelector('.popup__field_type_title');
-const imgLinkInput = document.querySelector('.popup__field_type_link-image');
-
-const nameInfo = document.querySelector('.profile__title');
-const jobInfo = document.querySelector('.profile__subtitle');
-
 const editButton = document.querySelector('.profile__edit-button');
-const closeEdit = formEdit.querySelector('.popup__close-button');
-
 const addButton = document.querySelector('.profile__add-button');
-const closeAdd = formAdd.querySelector('.popup__close-button');
-
-const photoContainerEl = document.querySelector('.photo-items');
-const templateEl = document.querySelector('.template');
-
-const popupBigPicture = document.querySelector('.popup_big-picture');
-const popupImg = document.querySelector('.popup__img');
-const popupDescription = document.querySelector('.popup__description');
-const closeImg = document.querySelector('.popup__close-button');
-
 const ESC_CODE = 'Escape';
-
-const submitButton = formAdd.querySelector('.popup__create-button');
 
 export {
   initialCards,
@@ -60,20 +39,7 @@ export {
   nameInput,
   jobInput,
   formAdd,
-  titleInput,
-  imgLinkInput,
-  nameInfo,
-  jobInfo,
   editButton,
-  closeEdit,
   addButton,
-  closeAdd,
-  photoContainerEl,
-  templateEl,
-  popupBigPicture,
-  popupImg,
-  popupDescription,
-  closeImg,
-  ESC_CODE,
-  submitButton
+  ESC_CODE
 };
